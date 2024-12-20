92b70afef5a92a702e03d1b6cb3a9ad35bedbfc9
StanIustina15 <iustina.stan@stud.ubbcluj.ro>
2024-12-20 22:07:21 +0200

    Add project files.

---
+++ Stan_Iustina_Lab11/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Stan_Iustina_Lab11.Data;
+
+namespace Stan_Iustina_Lab11
+
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ Stan_Iustina_Lab11/Data/IRestService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stan_Iustina_Lab11.Models;
+namespace Stan_Iustina_Lab11.Data
+{
+    public interface IRestService
+    {
+        Task DeleteShopListAsync(int iD);
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+
+        public interface IRestService
+        {
+            Task<List<ShopList>> RefreshDataAsync();
+            Task SaveShopListAsync(ShopList item, bool isNewItem);
+            Task DeleteShopListAsync(int id);
+        }
+    }
+}
